409fa21ea00e75ff916e56b095a3b9834140740d
zz1220 <xxxizz1220@gmail.com>
2020-04-07 01:10:48 -0700

    merge again

--- PysTest1/apps/psytests/apps.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-from django.apps import AppConfig
-
-
-class PsytestsConfig(AppConfig):
-    name = 'psytests'
-    verbose_name = u"心理测评"
--- PysTest1/apps/psytests/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class ReviewForm(forms.Form):
-    review = forms.CharField(label='review', max_length=100)
